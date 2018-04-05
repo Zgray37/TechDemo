8c4d0f5758075cbce7dcf60789e542f90953ddac
zgray37 <ugb4aevk>
2018-04-05 15:54:17 -0500

    Added code to MainActivity and CameraPreview classes. Also set up the layout.
    To fix:
    On click listeners for button_capture.

--- app/src/main/java/com/example/zacharygray14/techdemo/MainActivity.java
+++ app/src/main/java/com/example/zacharygray14/techdemo/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.zacharygray14.techdemo;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
+
+
 }
+
